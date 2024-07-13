b3c0f732e42678f038cb24aac8c4eb2cdf150618
leonne <minterbottsyt@gmail.com>
2024-07-13 22:11:42 +0300

    signup and login done v1

--- app.py
+++ app.py
@@ -1,51 +1,53 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager,create_access_token
 from flask_migrate import Migrate
 from config import Config
+from models import db
 
-
-
-db=SQLAlchemy()
-migrate=Migrate()
+migrate = Migrate()
 
 def create_app():
-    app=Flask(__name__)
+    app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
+    
     return app
 
-app=create_app()
+app = create_app()
 
-@app.route("/signup",methods=["POST"])
+@app.route("/signup", methods=["POST"])
 def create_user():
-    from models import User
-    body=request.get_json()
-    name=body('name')
-    email=body("email")
-    password=body("password")
-    user=User(name=name,email=email,password=password)
+    from models import User  
+    body = request.get_json()
+    name = body['name']
+    email = body['email']
+    password = body['password']
+    
+    user = User(name=name, email=email, password=password)
     db.session.add(user)
     db.session.commit()
-    return jsonify(user.to_dict()),201
+    
+    return jsonify({"message": "Successful request"}), 201
 
-@app.route('/login',methods=['POST'])
+@app.route("/login", methods=["POST"])
 def login():
+    body=request.get_json()
+    email=body['email']
+    password=body['password']
     from models import User
-    email=request.form['email']
-    password=request.form['password']
-    user = User.query.filter_by(email=email).first()
-    if not user:
-        return jsonify({'error':'user not found'}), 400
+
+    user=User.query.filter_by(email=email).first()
+    if user and user.password==password:
+        return jsonify({"message": "Login successful"}), 200
     else:
-        return jsonify({'message':'Success'}),201
-    
+        return jsonify({"message":"error"})
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-    
+
 
 
 
